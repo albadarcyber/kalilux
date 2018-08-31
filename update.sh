68fe1e17ea28d9a0face614a9140fb5629be7a7d
albadarcyber <42489512+albadarcyber@users.noreply.github.com>
2018-08-31 01:28:36 -0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pkg install php
+pkg install ruby
+pkg install curl
+pkg install git
+pkg install wget
+pkg install nodejs
+pkg install pip
